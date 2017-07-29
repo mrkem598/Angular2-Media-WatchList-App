0659ad361c1d85421cec7a3057f457569f690b5b
Moh Kemal <mohakemal9@gmail.com>
2017-07-29 13:11:43 -0400

    commiting application code

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+// bootstrap logic targeting the browser
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
